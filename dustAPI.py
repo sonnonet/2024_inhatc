f1ae3ff4393bd554ddc9cf4d2d480c4dcfaded99
sonnonet <korea.sonnonet@gmail.com>
2023-10-31 14:08:15 +0900

    Create dustAPI.py

---
+++ dustAPI.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode, quote_plus, unquote
+import requests
+
+url = 'https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty'
+queryParams = '?' + urlencode({quote_plus('serviceKey'):
+'Gkhq+uo4qB8ETdZCYbYJHIswWquBvkKR0PKmUHppucbBJztsGx17tq9cx56sDqItq3Ltd8wlQay9PP6yoyUmDQ=='
+                               ,quote_plus('returnType'): 'xml'
+                               ,quote_plus('numOfRows'): '10'
+                               ,quote_plus('pageNo'): '1'
+                               ,quote_plus('stationName'): '주안'
+                               ,quote_plus('dataTerm'): 'DAILY'
+                               ,quote_plus('ver'):'1.0'})
+
+res = requests.get(url+queryParams)
+soup = BeautifulSoup(res.content, 'html.parser')
+data = soup.find_all('item')
+print(data)
+               
