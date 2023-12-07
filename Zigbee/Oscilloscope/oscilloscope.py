1f64b04ee47ce6a369652d41c7d2cc6e09a30b47
sonnonet <korea.sonnonet@gmail.com>
2023-12-07 15:18:31 +0900

    Add files via upload

--- Zigbee/Oscilloscope/oscilloscope.py
+++ Zigbee/Oscilloscope/oscilloscope.py
@@ -14,6 +14,9 @@ import sys
 import tos
 import datetime
 import threading
+from influxdb import InfluxDBClient as influxdb
+import requests, json
+import time
 
 AM_OSCILLOSCOPE = 0x93
 
@@ -31,7 +34,7 @@ class OscilloscopeMsg(tos.Packet):
                              ],
                             packet)
 if '-h' in sys.argv:
-    print "Usage:", sys.argv[0], "serial@/dev/ttyUSB0:57600"
+    print ("Usage:", sys.argv[0], "serial@/dev/ttyUSB0:57600")
     sys.exit()
 
 am = tos.AM()
@@ -39,59 +42,46 @@ am = tos.AM()
 while True:
     p = am.read()
     msg = OscilloscopeMsg(p.data)
+    print(p)
 	
-####### CO2 Logic ############
-    if msg.type == 1:
-#	pbr_Num = pbrNum_return(msg.srcID)
-	pbr_Num = 3
-	CO2 = msg.Data0
-	CO2 = 1.5 * CO2 / 4096 * 2 * 1000
-	### MySQL Insert ###
-        try:
-            with conn.cursor() as curs:
-                Now = datetime.datetime.now()
-                sql = """insert into JB_Sensor_CO2(NODE_ID,SEQ,CO2,PBR_NUM,REGDATE)
-                        values(%s, %s, %s, %s, %s)"""
-                curs.execute(sql,(msg.srcID,msg.seqNo,CO2,pbr_Num,Now))
-                conn.commit()
-        except:
-            conn.close()
-        print "ID:",msg.srcID, "seqNo:",msg.seqNo, "CO2:",CO2
 ####### THL Logic ############
     if msg.type == 2:
         battery = msg.Data3
- #       battery = 0
 
         Illumi = int(msg.Data2)
         humi = -2.0468 + (0.0367*msg.Data1) + (-1.5955*0.000001)*msg.Data1*msg.Data1
         temp = -(39.6) + (msg.Data0 * 0.01)
-	    
-        print "id:" , msg.srcID, " Count : ", msg.seqNo, \
-                "Temperature: ",temp, "Humidity: ",humi, "Illumination: ",Illumi, "Battery : ", battery
 
-####### PH Sensor Logic #############
-    if msg.type == 3:
-        #hex1 = hex(msg.Data2)
-        #hex1 = hex1[:-1]
-        #hex2 = hex(msg.Data2_1)
-        #hex2 = hex2[:-1]
-#        print "before : ", PH_str
-#        print (int(hex1,0)), (int(hex2,0))
-#        print hex(msg.Data2), hex(msg.Data2_1)
-        
-        PH = msg.Data0
-        PH = ((float(PH) * 5)/4096)*3.5
-        PH = PH - 1
+        data = [{
+            'measurement' : 'office',
+            'tags':{
+                'seochang' : '10',
+            },
+            'fields':{
+                'Temperature' : temp,
+                'humidity' : humi,
+                'Lux' : Illumi,
+                'battery' : battery,
+            }
+        }]
+        client = None
+
         try:
-            with conn.cursor() as curs:
-                Now = datetime.datetime.now()
-                sql = """insert into JB_Sensor_PH(NODE_ID,PH,PBR_NUM,REGDATE) \
-                        values(%s, %s, %s, %s)"""
-                curs.execute(sql,(msg.srcID,PH,3,Now))
-                conn.commit()
-        except all,e:
-            print e.args
-            conn.close()
-        print "ID:",msg.srcID, "PH:",PH
-#       print PH
+            client = influxdb('localhost', 8086, 'root', 'root', 'THL')
+        except Exception as e:
+            print("Exception : " ,e)
+        if client is not None:
+            try:
+                client.write_points(data)
+                print("Influxdb insert OK")
+            except Exception as e:
+                print("Exception write_point ", e)
+            finally:
+                client.close()
+
+
+	    
+        print ("id:" , msg.srcID, " Count : ", msg.seqNo, \
+                "Temperature: ",temp, "Humidity: ",humi, "Illumination: ",Illumi, "Battery : ", battery)
+
 
