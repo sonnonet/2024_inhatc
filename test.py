2e865085cad6ffaa5d39a50ff317247edd01a26a
sonnonet <korea.sonnonet@gmail.com>
2025-02-12 15:25:48 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+class Human():
+    def __init__(self,name,age,se):
+        self.name = name
+        self.age = age
+        self.se = se
+
+areum = Human("아름",25,"여자")
+print(areum.name, areum.age, areum.se)
