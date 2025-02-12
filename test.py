33e4b0b92fd6a604f858a16bb3748ea22eec89b6
sonnonet <korea.sonnonet@gmail.com>
2025-02-12 15:26:11 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-class Human():
-    def __init__(self,name,age,se):
-        self.name = name
-        self.age = age
-        self.se = se
-
-areum = Human("아름",25,"여자")
-print(areum.name, areum.age, areum.se)
